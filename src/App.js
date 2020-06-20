b02a9d6017038054f40b544984b9d45151a189a2
katerinafaustenova <katerina.faustenova@seznam.cz>
2020-06-20 09:02:34 +0200

    dumb components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from "react";
 import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import Rank from "./components/Rank/Rank";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Particles from "react-particles-js";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 130,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      
+      <Particles className="particles" params={particlesOptions} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecoginition />*/}
     </div>
   );
 }
