e2befeb0e71e5d5d37990b1f22a152fd02067816
Kateřina Faustenová <55665611+katerinafaustenova@users.noreply.github.com>
2020-06-23 16:16:31 +0200

    link and button functionalities

--- src/App.js
+++ src/App.js
@@ -18,17 +18,37 @@ const particlesOptions = {
   }
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particles" params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecoginition />*/}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
+
+  onInputChange = event => {
+    console.log(event.target.value);
+  };
+
+  onSubmit = () => {
+    console.log("submitted");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onSubmit={this.onSubmit}
+        />
+        {/*<FaceRecoginition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
